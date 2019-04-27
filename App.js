9b44a2cfda39505d22af8530fbb62d10126a0d63
C3DMaX <C3DMaX@users.noreply.github.com>
2019-04-27 15:09:28 +0200

    main screen prep v 0.0.1
    added basic imports, states, constants and "pre camera" functions. no functionality yet

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Constants, Camera, FileSystem, Permissions} from 'expo';
+import { StyleSheet, Text, View, Alert, TouchableOpacity, Slider, Platform} from 'react-native';
+
+
+const flash = {off:'torch',torch:'off'};
+//todo: torch on/off icon logic and icon assets
 
 export default class App extends React.Component {
+
+  state = {
+    //todo: camera prop defaults
+    camPermission:false,
+    ratios:[],
+  }
+
+//check for camera Permissions
+
+  async componentWillMount() {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({camPermission: status === 'granted'});
+  }
+
+//get supported viewfinder ratios
+
+  getRatios = async => {
+    const ratios = await this.camera.getSupportedRatios();
+    return ratios;
+  }
+
+//todo: implement camera as function
+
   render() {
     return (
+
+      //todo: check permission status and call camera function if permission is granted
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome to Waypoint!</Text>
       </View>
     );
   }
